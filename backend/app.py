3386e129f67d95801a095fdb44829d4fa1d9da97
DhruvBShetty <dhruvbshetty@gmail.com>
2024-12-16 18:05:32 +0200

    Allow from anywhere

--- backend/app.py
+++ backend/app.py
@@ -26,7 +26,7 @@ supabase= create_client(url, key)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[""],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True
