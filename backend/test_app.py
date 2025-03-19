7b14a7bbab7d21b378c70b614f5bc4e46472b6f8
DhruvBShetty <dhruvbshetty@gmail.com>
2025-03-19 18:09:30 +0200

    refactoring test

--- backend/test_app.py
+++ backend/test_app.py
@@ -20,6 +20,8 @@ supabase= create_client(url, key)
 
 url= os.environ.get("SUPABASE_URL")
 key= os.environ.get("SUPABASE_KEY")
+uidpass=os.environ.get("UIDPASS")
+uid2pass=os.environ.get("UID2PASS")
 
 def test_read_app():
     response = client.get("/")
@@ -27,16 +29,14 @@ def test_read_app():
     assert response.json() == {"message": "Welcome to Eternitas"}
 
 
-# def test_priv():
-#     try:
-#         memo_info=supabase.table("Memorial_info").select("id,First_name,Middle_name,Last_name,Date_of_birth,Date_of_death,Relationship,Description,Profile_pic,Privacy").eq("id",2).execute()
+def test_priv():
+    try:
+        memo_info=supabase.table("Memorial_info").select("id,First_name,Middle_name,Last_name,Date_of_birth,Date_of_death,Relationship,Description,Profile_pic,Privacy").eq("id",2).execute()
+    except APIError as e:
+        raise HTTPException(status_code=400, detail=e.details)  
+    data=memo_info.data
     
-#     except APIError as e:
-#         raise HTTPException(status_code=400, detail=e.details)
-    
-#     data=memo_info.data
-    
-#     assert data[0]["Privacy"]==True
+    assert data[0]["Privacy"]==True
 
 
 
@@ -54,31 +54,6 @@ def test_verify_cookie():
     assert response.status_code==200
 
 
-def test_set_cookie():
-    
-    uid:int=2
-        
-    guest_id=uuid.uuid4()
-    payload={"anon_id":str(guest_id),"Pages":"[26]"}
-    token=jwt.encode(payload,et_key,algorithm="HS256")
-    client.cookies.set(name="Eternitas_pages",value=token)
-
-    response = client.post("/api/setpcookie", json={"uid":uid})
-    token=response.cookies["Eternitas_pages"]
-
-    try:
-        payload=jwt.decode(token,et_key,algorithms=["HS256"])
-    except Exception as e:
-        raise HTTPException(status_code=401,detail=str(e))
-    
-    pages_list = json.loads(payload.get('Pages'))
-
-    print(pages_list)
-
-    if uid in pages_list:
-        assert True
-    else:
-        assert False
 
 def test_set_cookie_withpass():
     
@@ -86,11 +61,11 @@ def test_set_cookie_withpass():
     uid2:int=3
         
     guest_id=uuid.uuid4()
-    payload={"anon_id":str(guest_id),"Pages":"[26]"}
+    payload={"anon_id":str(guest_id),"Pages":"[2]"}
     token=jwt.encode(payload,et_key,algorithm="HS256")
     client.cookies.set(name="Eternitas_pages",value=token)
 
-    response = client.post("/api/setpcookie", json={"uid":uid,"password":"password"})
+    response = client.post("/api/setpcookie", json={"uid":uid,"password":uidpass})
 
     token=response.cookies["Eternitas_pages"]
     try:
@@ -105,14 +80,14 @@ def test_set_cookie_withpass():
     else:
         assert False
     
-    response2 = client.post("/api/setpcookie",json={"uid":uid2,"password":"pass"})
+    response2 = client.post("/api/setpcookie",json={"uid":uid2,"password":uid2pass})
 
     assert response2.status_code==401
 
 
 def test_public_profdata():
 
-    uid:int=2
+    uid:int=29
         
     guest_id=uuid.uuid4()
     payload={"id":uid}
@@ -120,9 +95,9 @@ def test_public_profdata():
     
     client.cookies.set(name="Eternitas_session",value=token)
 
-    response=client.post("/api/profiledata/2")
+    response=client.post("/api/profiledata/29")
 
-    print(response.json())
+    assert response.status_code==200
 
 
 
